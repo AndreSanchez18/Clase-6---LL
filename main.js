a93a8491edd26b735ebb8e2f334933135e6a8376
andres <andressanchez1816@gmail.com>
2025-05-08 20:04:35 -0300

    agregar try-catch

--- main.js
+++ main.js
@@ -34,12 +34,21 @@ app.use(express.json())
 PORT = 3000
 
 app.post("/operaciones", (req,res)=>{
-    let result = req.body.map(e=>{
-        const funcion = funciones.find(o=>o.name==e.fn)
-        return {...e, resultado:funcion.value(e.op1, e.op2)}
-    })
 
-    res.status(201).json(result)
+    try{
+        let result = req.body.map(e=>{
+            const funcion = funciones.find(o=>o.name==e.fn)
+            if (!funcion) throw new Error("Operacion no soportada")
+            return {...e, resultado:funcion.value(e.op1, e.op2)}
+        })
+
+        res.status(201).json(result)
+
+    } catch(err){
+        res.status(500).json({
+            message:err.message
+        })
+    }
 })
 
 app.listen(PORT)
